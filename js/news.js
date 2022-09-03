5e8ee664058c7dd2af1537d8263fa5c1db7f590e
mrseeker53 <mi.nayeem30@gmail.com>
2022-09-03 19:42:54 +0600

    updated category id

--- js/news.js
+++ js/news.js
@@ -10,15 +10,28 @@ const loadCategory = async () => {
 const displayCategory = (categories) => {
     const categoryContainer = document.getElementById('category-container');
     categories.forEach(category => {
-        console.log(category)
         const categoryLi = document.createElement('li');
         categoryLi.classList.add('nav-item')
         categoryLi.innerHTML = `
-        <a class="nav-link text-secondary" href="#${category.category_name}">${category.category_name}</a>
+        <a onclick="displayNews('${category.category_id}')" class="nav-link text-secondary" href="#${category.category_name}">${category.category_name}</a>
         `;
         categoryContainer.appendChild(categoryLi);
     })
 
+
 }
+
+const displayNews = async (categoryId) => {
+    console.log(categoryId)
+    const url = `https://openapi.programming-hero.com/api/news/category/${categoryId}`
+    console.log(url)
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data)
+
+}
+
+
+
 
 loadCategory();
