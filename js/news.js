e8682ff761e859c66807d204dee81140138c8baa
mrseeker53 <mi.nayeem30@gmail.com>
2022-09-04 08:44:26 +0600

    final update

--- js/news.js
+++ js/news.js
@@ -2,7 +2,7 @@
 const loadCategory = async () => {
     // error handler
     try {
-        const url = 'https://openapi.programming-hero.com/api/news/categories'
+        const url = 'https://openapi.programming-hero.com/api/news/categories';
         const res = await fetch(url);
         const data = await res.json();
         displayCategory(data.data.news_category);
@@ -17,7 +17,7 @@ const displayCategory = (categories) => {
     const categoryContainer = document.getElementById('category-container');
     categories.forEach(category => {
         const categoryLi = document.createElement('li');
-        categoryLi.classList.add('nav-item')
+        categoryLi.classList.add('nav-item');
         categoryLi.innerHTML = `
         <a onclick="displayNews('${category.category_id}')" class="nav-link text-secondary" href="#${category.category_name}">${category.category_name}</a>
         `;
@@ -29,18 +29,17 @@ const displayCategory = (categories) => {
 const displayNews = async (categoryId) => {
     // start spinner
     toggleSpinner(true);
-    const url = `https://openapi.programming-hero.com/api/news/category/${categoryId}`
+    const url = `https://openapi.programming-hero.com/api/news/category/${categoryId}`;
     const res = await fetch(url);
     const data = await res.json();
-    displayNewsDatails(data.data)
+    displayNewsDatails(data.data);
 }
 
 const displayNewsDatails = async (newsId) => {
     // display total news
     const totalNews = document.getElementById('total-news');
     totalNews.innerHTML = `
-    <h5>${newsId.length ? newsId.length :
-            'No'} News Found</h5>
+    <h5>${newsId.length ? newsId.length : 'No'} News Found</h5>
     `;
 
     // most viewed news display first
@@ -58,16 +57,16 @@ const displayNewsDatails = async (newsId) => {
             <div class="col-md-4">
                 <img src="${news.thumbnail_url}" class="img-fluid rounded" alt="...">
             </div>
-            <div class="col-md-8">
+            <div class="col-md-8 col-xs-1">
                 <div class="card-body">
                     <h5 class="card-title">${news.title}</h5>
                     <p class="card-text">${news.details.slice(0, 500)}...</p>
-                </div>
-                <div class="col-4 d-flex align-items-center px-3 pt-3">
-                    <img class="img-fluid rounded w-25 me-3" src="${news.author.img}"
-                    <h5 class="pe-5 me-5">${news.author.name}</h5>
-                    <span class="px-5 mx-5">${news.total_view}</span>
-                    <button onclick="loadNewsInfo('${news._id}')" class="btn btn-primary px-5" data-bs-toggle="modal" data-bs-target="#newsInfoModal">Info</button>
+                    <div class="d-flex align-items-center px-3 pt-3">
+                        <img style="height: 60px; width: 60px;" class="img-fluid rounded me-3" src="${news.author.img}"
+                        <h5 class="pe-5 me-5">${news.author.name}</h5>
+                        <span class="px-5 mx-5">${news.total_view}</span>
+                        <button onclick="loadNewsInfo('${news._id}')" class="btn btn-primary px-5 d-flex ms-auto" data-bs-toggle="modal" data-bs-target="#newsInfoModal">Info</button>
+                    </div>
                 </div>
             </div>
         </div>
@@ -80,10 +79,10 @@ const displayNewsDatails = async (newsId) => {
 }
 
 const loadNewsInfo = async (newsId) => {
-    const url = `https://openapi.programming-hero.com/api/news/${newsId}`
+    const url = `https://openapi.programming-hero.com/api/news/${newsId}`;
     const res = await fetch(url);
     const data = await res.json();
-    displayNewsInfo(data.data[0])
+    displayNewsInfo(data.data[0]);
 }
 
 // view info using modal
@@ -95,7 +94,7 @@ const displayNewsInfo = (info) => {
     <p>Author Name: ${info.author.name ? info.author.name : 'Data Not Available'}</p>
     <p>Published Date: ${info.author.published_date ? info.author.published_date : 'Data Not Available'}</p>
     <p>Total View: ${info.total_view ? info.total_view : 'Data Not Available'}</p>
-    `
+    `;
 }
 
 // spinner
@@ -110,3 +109,5 @@ const toggleSpinner = isLoading => {
 }
 
 loadCategory();
+
+displayNews('08');
